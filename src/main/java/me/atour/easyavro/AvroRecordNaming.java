9231fc1836b2dfbc2f796e66ccb4c831c85e4b0f
amousavigourabi <28668597+amousavigourabi@users.noreply.github.com>
2023-07-06 14:18:36 +0200

    Add naming strategy annotation

---
+++ src/main/java/me/atour/easyavro/AvroRecordNaming.java
@@ -0,0 +1,20 @@
+package me.atour.easyavro;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AvroRecordNaming {
+
+  enum FieldNamingStrategies {
+    SNAKE_CASE,
+    KEBAB_CASE,
+    DROMEDARY_CASE,
+    UPPERCASE,
+    LOWERCASE,
+    SCREAMING_SNAKE_CASE,
+    PASCAL_CASE
+  }
+
+  FieldNamingStrategies strategy() default FieldNamingStrategies.DROMEDARY_CASE;
+}
